45ab8971566c52c72526354f56b6418fcf89ed73
Azizur Rahman <azizur.rahman@bbc.co.uk>
2016-03-22 16:00:26 +0000

    Wiring up Widgets and Getting references to widgets

--- app/src/main/java/com/azizur/geoquiz/QuizActivity.java
+++ app/src/main/java/com/azizur/geoquiz/QuizActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,9 @@ public class QuizActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton= (Button) findViewById(R.id.false_button);
     }
 
     @Override
